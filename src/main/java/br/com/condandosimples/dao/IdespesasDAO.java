d043ff192233a99a4ba88b4dc663cc4c4efc0156
Maiagit2024 <j.sadko27@gmail.com>
2024-11-05 21:15:56 -0300

    Update IdespesasDAO.java

--- src/main/java/br/com/condandosimples/dao/IdespesasDAO.java
+++ src/main/java/br/com/condandosimples/dao/IdespesasDAO.java
@@ -1,2 +1,20 @@
-package br.com.condandosimples.dao;public interface IdespesasDAO {
+package br.com.condandosimples.dao;
+
+import br.com.condandosimples.model.Categoria;
+import br.com.condandosimples.model.Despesas;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IdespesasDAO {
+
+    Despesas save (Despesas despesas);
+    boolean update (Despesas despesas);
+    boolean delete (Long id);
+    List<Despesas> findALL();
+    Optional<Despesas> findById(Long id);
+    List<Despesas> findCategoria(Categoria categoria);
+
+
+
 }
