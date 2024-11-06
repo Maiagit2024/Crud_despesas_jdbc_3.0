2be5954f11c6ba408b4f4b62dfad73da87109eb7
Maiagit2024 <j.sadko27@gmail.com>
2024-11-05 21:19:57 -0300

    Update Despesas.java

--- src/main/java/br/com/condandosimples/model/Despesas.java
+++ src/main/java/br/com/condandosimples/model/Despesas.java
@@ -1,2 +1,53 @@
-package br.com.condandosimples.model;public class despesas {
+package br.com.condandosimples.model;
+
+import java.time.LocalDate;
+
+public class Despesas {
+
+    private Long id;
+    private String descricao;
+    private LocalDate data;
+    private double valor;
+    private Categoria categoria;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
